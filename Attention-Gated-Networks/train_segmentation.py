73bf9e4bc2d6fb6408489be4701ba355fb0fabfb
Brice Rauby <brice.rauby@student.ecp.fr>
2020-03-24 23:44:01 +0100

    cleaning dataio part

--- Attention-Gated-Networks/train_segmentation.py
+++ Attention-Gated-Networks/train_segmentation.py
@@ -1,10 +1,11 @@
-import numpy
+import os
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 
-from dataio.loader import get_dataset, get_dataset_path
-from dataio.transformation import get_dataset_transformation
+
+import numpy as np
+from dataio.LiTS import LiTSDataset
 from utils.util import json_file_to_pyobj
 from utils.visualiser import Visualiser
 from utils.error_logger import ErrorLogger
@@ -24,10 +25,27 @@ def train(arguments):
     # Architecture type
     arch_type = train_opts.arch_type
 
-    # Setup Dataset and Augmentation
-    ds_class = get_dataset(arch_type)
-    ds_path  = get_dataset_path(arch_type, json_opts.data_path)
-    ds_transform = get_dataset_transformation(arch_type, opts=json_opts.augmentation)
+    # Setup Split
+    data_path = json_opts.data_path.LiTS
+    print('data_path ', data_path)
+    SEED = 0 
+    np.random.seed(SEED)
+    image_dir = os.listdir(os.path.join(data_path,'Training Batch 1')) + os.listdir(os.path.join(data_path,'Training Batch 2'))
+    all_indexes = [ int(file_name[7:-4]) for file_name in image_dir if 'volume' in file_name]
+    split = np.random.permutation(all_indexes)
+    n_train, n_val, n_test = int(0.8 * len(split)), int(0.1 * len(split)), int(0.1 * len(split))
+    
+    train = split[: n_train]
+    val = split[n_train : n_train+n_val]
+    test = split[n_train + n_val :]
+
+    # Setup Data Loader
+    train_dataset = LiTSDataset(data_path, train, augment=True)
+    val_dataset = LiTSDataset(data_path, val)
+    test_dataset = LiTSDataset(data_path, test)
+    train_loader = DataLoader(dataset=train_dataset, num_workers=16, batch_size=train_opts.batchSize, shuffle=True)
+    valid_loader = DataLoader(dataset=val_dataset, num_workers=16, batch_size=train_opts.batchSize, shuffle=False)
+    test_loader  = DataLoader(dataset=test_dataset,  num_workers=16, batch_size=train_opts.batchSize, shuffle=False)
 
     # Setup the NN Model
     model = get_model(json_opts.model)
@@ -35,15 +53,6 @@ def train(arguments):
         print('# of pars: ', model.get_number_parameters())
         print('fp time: {0:.3f} sec\tbp time: {1:.3f} sec per sample'.format(*model.get_fp_bp_time()))
         exit()
-
-    # Setup Data Loader
-    train_dataset = ds_class(ds_path, split='train',      transform=ds_transform['train'], preload_data=train_opts.preloadData)
-    valid_dataset = ds_class(ds_path, split='validation', transform=ds_transform['valid'], preload_data=train_opts.preloadData)
-    test_dataset  = ds_class(ds_path, split='test',       transform=ds_transform['valid'], preload_data=train_opts.preloadData)
-    train_loader = DataLoader(dataset=train_dataset, num_workers=16, batch_size=train_opts.batchSize, shuffle=True)
-    valid_loader = DataLoader(dataset=valid_dataset, num_workers=16, batch_size=train_opts.batchSize, shuffle=False)
-    test_loader  = DataLoader(dataset=test_dataset,  num_workers=16, batch_size=train_opts.batchSize, shuffle=False)
-
     # Visualisation Parameters
     #visualizer = Visualiser(json_opts.visualisation, save_dir=model.save_dir)
     error_logger = ErrorLogger()
