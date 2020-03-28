6a02ac0de872803e49087be63264bea66467c48e
S.Brandeis <s.brandeis@orange.fr>
2020-03-28 12:06:18 +0100

    Add other ResNet architectures

--- models/backbone/resnet.py
+++ models/backbone/resnet.py
@@ -3,19 +3,32 @@ from torch import nn
 from torch.nn import functional as F
 
 from models.backbone.backbone import Backbone
-from models.blocks.resnet import BasicBlock, conv1x1, conv3x3
+from models.blocks.resnet import BasicBlock, Bottleneck, conv1x1, conv3x3
 
 
-class ResNet3D18(Backbone):
-    def __init__(self, in_channels,
+class ResNet3D(Backbone):
+    DEPTHS = {
+        18: (BasicBlock, [2, 2, 2, 2]),
+        34: (BasicBlock, [3, 4, 6, 3]),
+        50: (Bottleneck, [3, 4, 6, 3]),
+        101: (Bottleneck, [3, 4, 23, 3]),
+    }
+
+    def __init__(self, in_channels, depth=18,
                  groups=1, width_per_group=64):
         """
         MobileNet V2 main class
         Args:
             in_channels (int): Number of input channels
         """
-        super(ResNet3D18, self).__init__(in_channels=in_channels)
-        layers = [2, 2, 2, 2]
+        super(ResNet3D, self).__init__(in_channels=in_channels)
+        self.depth = depth
+        if self.depth in self.DEPTHS:
+            block, layers = self.DEPTHS[self.depth]
+        else:
+            print("Unknown depth; defaulting to 18")
+            block, layers = self.DEPTHS[18]
+
         self.inplanes = 64
 
         self.groups = groups
@@ -26,10 +39,10 @@ class ResNet3D18(Backbone):
         self.bn1 = nn.BatchNorm3d(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool3d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(BasicBlock, 64, layers[0])
-        self.layer2 = self._make_layer(BasicBlock, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(BasicBlock, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(BasicBlock, 512, layers[3], stride=2)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
 
         self.llf_channels = 128*BasicBlock.expansion
         self.hlf_channels = 512*BasicBlock.expansion
