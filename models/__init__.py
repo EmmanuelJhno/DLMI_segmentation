0f2cf6182e4cb37d2752144fdd74565656018d42
S.Brandeis <s.brandeis@orange.fr>
2020-03-28 17:04:36 +0100

    Add config to run Attention Gated UNet

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,12 @@
 from models.deeplab import DeepLab3D
 from models.unet import Unet3D
+from models.attention_gated_unet import AttentionGatedUnet3D
 
 
 def get_model(config):
     if config.name == 'unet':
         return Unet3D(config)
+    if config.name == "attention_unet":
+        return AttentionGatedUnet3D(config)
     if config.name == 'deeplab':
         return DeepLab3D(config)
