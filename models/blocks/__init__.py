a3abdeaa840f6f618ac47c331d299eae41e6317b
S.Brandeis <s.brandeis@orange.fr>
2020-03-28 16:59:20 +0100

    Impl Multi-attention CT Unet with Deep Supervision

--- models/blocks/__init__.py
+++ models/blocks/__init__.py
@@ -1,4 +1,5 @@
 from models.blocks.convbn3d import Conv3dBNReLU
 from models.blocks.inverted_residual import InvertedResidual
 from models.blocks.aspp3d import ASPP3d, ASPPConv3d, ASPPPooling3d
-from models.blocks.unet import UnetConv3, UnetUp3
+from models.blocks.unet import UnetConv3, UnetUp3, UnetUp3_CT, UnetGridGatingSignal3, UnetDsv3
+from models.blocks.grid_attention import MultiAttentionBlock
