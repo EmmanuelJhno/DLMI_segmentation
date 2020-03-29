75d00c6cf0319132b7acd2155cfdbd6ab74eb654
Brice Rauby <brice.rauby@student.ecp.fr>
2020-03-29 18:00:22 +0200

    error analysis script

--- utils/error_analysis.py
+++ utils/error_analysis.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import SimpleITK as sitk
 import tqdm
 import pandas as pd 
-from PIL import Image
+import cv2
 import torch
 import torch.nn as nn
 import numpy as np
@@ -19,7 +19,7 @@ from torch.utils.data import DataLoader
 from models import get_model
 from data_loader.LiTS import LiTSDataset
 from utils.train import ObjFromDict
-
+from skimage.measure import label
 
 def load_model(run_dir, metric='validation_dice'): 
     with open(os.path.join(run_dir,'config.json')) as json_file:
@@ -31,6 +31,10 @@ def load_model(run_dir, metric='validation_dice'):
     return model, config
 
 
+def compute_dice(gt, pred): 
+    eps = 1e-5
+    intersection = np.sum(gt * pred)
+    return ((2*  intersection) + eps)/ (eps + np.sum(gt+pred))
 
           
 def main(raw_args=None): 
@@ -38,15 +42,14 @@ def main(raw_args=None):
     parser = argparse.ArgumentParser()    
     parser.add_argument('--run_dir', required=True, 
                         type=str,help = 'path to the run directory to perform the error analysis on')
-    parser.add_argument('--error_analysis', required=True, 
-                        type=str,help = 'path to the run directory to perform the error analysis on')
-    parser.add_argument('--test_set_path', default=None, 
-                        type=str,help = 'path to the test to perform the inference on')
-    parser.add_argument('--submision_path', default=None, 
-                        type=str,help = 'path to the folder to store the submission')
-    
+    parser.add_argument('--post_process', action='store_true', default=False,
+                        help='whether to use postprocessing or not')
+    parser.add_argument('--device', default='cuda:0', 
+                        type=str,help = 'device to use')
+
     args = parser.parse_args(raw_args)
     
+    device=args.device
     run_dir = args.run_dir
     model, config = load_model(run_dir)
 
@@ -54,165 +57,151 @@ def main(raw_args=None):
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
         
-    config.training.batch_size = 32
-    train_dataloader, val_dataloader = setup_dataset(config)
+    config.training.batch_size = 1
     
-    if not recompute: 
-        try: 
-            result = pd.read_csv(os.path.join(output_dir, 'val_pred.csv'))
-            print('Predictions recovered from previous inference')
-        except: 
-            print('Not able to recover predictions from previous inference, starting inference again ...')
-            result = infer(model, val_dataloader,os.path.join(output_dir, 'val_pred.csv'))
-    else:
-        result = infer(model, val_dataloader,os.path.join(output_dir, 'val_pred.csv'))
-        
-    occ =  train_dataloader.dataset.occ
-    occ = pd.DataFrame(occ, index=None).reset_index()
-    occ = occ.rename({'index':'label','label':'occ'}, axis=1)
-    results_with_occ = pd.merge(result,occ, on='label', how='inner')
-    unique_occ = occ['occ'].unique()
+    data_path = config.dataset.root
+    print('data_path ', data_path)
+
+    # fix the seed for the split
+    split_seed = 0 
+    np.random.seed(split_seed)
+
+    image_dir = os.listdir(os.path.join(data_path,'Training Batch 1')) + os.listdir(os.path.join(data_path,'Training Batch 2'))
+    all_indexes = [ int(file_name[7:-4]) for file_name in image_dir if 'volume' in file_name]
+    split = np.random.permutation(all_indexes)
+    n_train, n_val, n_test = int(0.8 * len(split)), int(0.1 * len(split)), int(0.1 * len(split))
+
+    train = split[: n_train]
+    val = split[n_train : n_train+n_val]
+    test = split[n_train + n_val :]
     
-    create_acc_per_occ_curve(output_dir, unique_occ, results_with_occ, 1)
-    create_acc_per_occ_curve(output_dir, unique_occ, results_with_occ, 3)
+    batch_size=1
     
-    if not recompute: 
-        try: 
-            embeddings, labels = np.load(os.path.join(output_dir, 'embedding.npy')), np.load(os.path.join(output_dir, 'labels.npy'))
-            print('Embeddings recovered from previous inference')
-            try:
-                tnse_embedding = np.load(os.path.join(output_dir, 'tnse_embedding.npy'))
-                print('TNSE embeddings recovered from previous inference')
-            except: 
-                print('Not able to recover TNSE embeddings from previous inference')
-                tnse_embedding = get_tnse(embeddings, 
-                                          os.path.join(output_dir, 'tnse_embedding.npy'), 
-                                          os.path.join(output_dir,'pca_explained_ratio.jpg'))
-        except: 
-            print('Not able to recover embeddings from previous inference, starting inference again ...')
-            embeddings, labels = get_embedding(model, 
-                                               val_dataloader, 
-                                               os.path.join(output_dir, 'embedding.npy'),
-                                               os.path.join(output_dir, 'labels.npy'))
-            tnse_embedding = get_tnse(embeddings, 
-                                      os.path.join(output_dir, 'tnse_embedding.npy'), 
-                                      os.path.join(output_dir,'pca_explained_ratio.jpg'))
-    else:
-        embeddings, labels = get_embedding(model,
-                                           val_dataloader,
-                                           os.path.join(output_dir, 'embedding.npy'),
-                                           os.path.join(output_dir, 'labels.npy'))
-        tnse_embedding = get_tnse(embeddings,
-                                  os.path.join(output_dir, 'tnse_embedding.npy'),
-                                  os.path.join(output_dir,'pca_explained_ratio.jpg'))
     
-    for label in highlighted_classes: 
-        fg, ax = plt.subplots(1,1)
-        ax.set_title('TNSE embedding class {} vs others \n n train samples : {} | top 1 acc : {:.3f}'.format(label, 
-                                                                occ.loc[occ['label']==1]['occ'].values[0],
-                                                                get_accuracy(results_with_occ.loc[results_with_occ['label']==label],1)))
-        ax.scatter(tnse_embedding[:,0], tnse_embedding[:,1],s=0.05, c=1-(labels==label) )
-        fg.savefig(os.path.join(output_dir, 'tnse_class_{}.jpg'.format(label)))
-        
-        
-    # Add a Grad-CAM visualization to interpret our model
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    trunc_model = nn.Sequential(*list(model.children())[:-2])
-    trunc_model.eval()
+    model = model.to(device)
+    model.eval()
+    
+    #### add test for physical reference size
+    dataset = LiTSDataset(data_path, test, 
+                          physical_reference_size = (512, 512, 512), 
+                          spacing=2,
+                          bounding_box=[0,1,0,1,0,1],
+                          no_tumor=True, 
+                          inference_mode=True)
+    dataloader = DataLoader(dataset=dataset, num_workers=0, batch_size=batch_size, shuffle=False)
+    
+    dices = []
     with torch.no_grad():
-        empty = torch.zeros((1,3,400,400))
-        try:
-            model_out_size = trunc_model(empty).shape[-1]
-        except:
-            empty = empty.to(device)
-            model_out_size = trunc_model(empty).shape[-1]
-        
-    last_conv_layer = list(model.children())[-3][-1][-1].conv2
-    for index in range(5):
-        sample, infos = val_dataloader.dataset[index]
-        sample = torch.from_numpy(sample).view(1,3,400,400)
-        # 'sample' is the preprocessed image
+        for batch_idx, (data, target) in tqdm.tqdm(enumerate(dataloader)):
 
-        # Load original image preprocessed without normalization
-        try:
-            path = infos['file_name']
-            img_path = os.path.join(os.path.join(config.dataset.root, 'train_images'), path)
-        except:
-            path = infos['path']
-            img_path = os.path.join(config.dataset.image_dir, path)
-        input_image = Image.open(img_path).convert('RGB')
-        input_image = transforms.Resize(400)(input_image)
+                data = data.to(device)
+                output = model(data)
+    #             output = target["one_hot_target"]
+                for i in range(batch_size):
+                    img = sitk.ReadImage(target['original_image_path'][i])
+                    normalization_transform = dataset.get_normalization_transform(img)
+                    inv_normalization_transform = normalization_transform.GetInverse()
+                    out_mask = np.round(output.cpu().numpy()[i,1,:,:,:])
 
-        with torch.no_grad():
-            try:
-                output = model(sample)
-            except:
-                sample = sample.to(device)
-                output = model(sample)
-            values, indices = torch.topk(output, 3)
+                    bb = target['bounding_box'].cpu().numpy()[i]
 
-        fig = plt.figure(figsize=(12, 3))
-        fig.suptitle("Heatmaps for image #{}".format(index), y=1.10)
-        ax = fig.add_subplot(141)
-        ax.set_title("Original Image")
-        ax.imshow(input_image)
-        #ax.set_xticks(None)
-        #ax.set_yticks(None)
+                    ref_img = sitk.Resample(img, dataset.reference_image, normalization_transform)
 
-        for i, target_class in enumerate(indices[0]):
-            weights, conv_maps = compute_grad_weights(model, last_conv_layer, target_class, sample, device)
-            coarse_heatmap, heatmap = compute_heatmap(conv_maps, weights, model_out_size, device)
-            ax = fig.add_subplot(142 + i)
-            title = "Class #{} predicted".format(target_class.item())
-            ax.set_title(title)
-            ax.imshow(input_image)
-            try:
-                ax.imshow(heatmap.numpy()[0, 0], cmap="jet", alpha=0.5)
-            except:
-                ax.imshow(heatmap.cpu().numpy()[0, 0], cmap="jet", alpha=0.5)
-            #ax.set_xticks(None)
-            #ax.set_yticks(None)
-            
-        fig.savefig(os.path.join(output_dir, 'grad_cam_{}.jpg'.format(index)))
+                    big_mask_numpy = sitk.GetArrayFromImage(ref_img)
+                    big_mask_numpy[:,:,:] = 0
+                    big_mask_numpy[bb[0]:bb[1],bb[2]:bb[3],bb[4]:bb[5]] = out_mask
+
+                    out_mask_image = sitk.GetImageFromArray(big_mask_numpy)
+                    out_mask_image.SetOrigin(ref_img.GetOrigin())
+                    out_mask_image.SetDirection(ref_img.GetDirection())
+                    out_mask_image.SetSpacing(ref_img.GetSpacing())
+                    out_mask_image_original_space = sitk.Resample(out_mask_image, 
+                                                                  img, 
+                                                                  inv_normalization_transform, 
+                                                                  sitk.sitkNearestNeighbor)
+                    original_mask = sitk.ReadImage(target['original_mask_path'][i])
+                    original_mask_array = np.clip(sitk.GetArrayFromImage(original_mask),0,1)
+                    out_mask_image_original_space_array = np.clip(sitk.GetArrayFromImage(out_mask_image_original_space),0,1)
+                    if args.post_process:
+                        labels, num = label(out_mask_image_original_space_array, return_num=True)
+                        one_hot_labels = np.zeros(labels.shape + (1+num,))
+                        labels = np.expand_dims(labels, axis=-1)
+                        one_hot_labels = (labels == np.arange(1+num).reshape(1, 1 + num)).astype('int')
+                        biggest_component_idx = np.argsort(np.sum(one_hot_labels, axis=(0,1,2)))[-2]
+                        out_mask_image_original_space_array = one_hot_labels[:,:,:,biggest_component_idx]
+                    dice = compute_dice(out_mask_image_original_space_array, original_mask_array)
+                    dices.append(dice)
+                    print(dice)
+                    img_array = np.clip(sitk.GetArrayFromImage(img),-200,250)
+                    fg, ax = plt.subplots(3,3,figsize=(10,10))
+                    h,w,d = original_mask_array.shape
+                    best_slice_h = np.argmax(np.sum(np.abs(out_mask_image_original_space_array - 
+                                                           original_mask_array), 
+                                                    axis=(1,2)))
+                    best_slice_w = np.argmax(np.sum(np.abs(out_mask_image_original_space_array - 
+                                                           original_mask_array), 
+                                                    axis=(0,2)))
+                    best_slice_d = np.argmax(np.sum(np.abs(out_mask_image_original_space_array - 
+                                                           original_mask_array), 
+                                                    axis=(0,1,)))
+                    ax[0,0].set_title('original image')
+                    ax[0,1].set_title('ground truth')
+                    ax[0,2].set_title('prediction')
+#                     img = np.rot90(img_array[:,:,best_slice_d],2)
+#                     img = cv2.resize(img,dsize=(w,d))
+                    ax[0,0].imshow(np.rot90(img_array[:,:,best_slice_d],2))
+                    ax[1,0].imshow(np.rot90(img_array[:,best_slice_w ,:],2))
+                    ax[2,0].imshow(np.rot90(img_array[best_slice_h,:,:],2))
+                    ax[0,1].imshow(np.rot90(original_mask_array[:,:,best_slice_d],2))
+                    ax[1,1].imshow(np.rot90(original_mask_array[:,best_slice_w ,:],2))
+                    ax[2,1].imshow(np.rot90(original_mask_array[best_slice_h,:,:],2))
+                    ax[0,2].imshow(np.rot90(out_mask_image_original_space_array[:,:,best_slice_d],2))
+                    ax[1,2].imshow(np.rot90(out_mask_image_original_space_array[:,best_slice_w ,:],2))
+                    ax[2,2].imshow(np.rot90(out_mask_image_original_space_array[best_slice_h,:,:],2))
+                    fg.suptitle('prediction {} dice : {}'.format(test[int(batch_idx*batch_size+i)], dice))
+                    plt.savefig(os.path.join(output_dir,'prediction_{}.png'.format(test[int(batch_idx * batch_size + i)])))
+                    
+        print('mean dice : ', np.mean(dices))
+        print('dices',dices)
         
         
-    with open(os.path.join(output_dir, 'error_analysis.html'),'w') as file: 
-        file.write('<!DOCTYPE html>\n'
-                   '<html lang="fr">\n'
-                   '  <head>\n'
-                   '    <meta charset="utf-8">\n'
-                   '    <title>Error_Analysis | {}</title>\n'
-                   '  </head>\n'
-                   '  <body>\n'
-                   '    <div> \n'
-                   '       <h1> Error_Analysis | {} </h1>\n'                   
-                   '       <h2> Global Metrics : </h2>\n'
-                   '        <p> Acc: {} <br /> Top3 acc : {}  <br /> '
-                   '       </p>\n'
-                   '    </div>\n'.format(run_dir.split('/')[-1],
-                                         run_dir.split('/')[-1],
-                                         get_accuracy(result,1), get_accuracy(result,3)
-                                        ))
-        file.write('    <div> \n'
-                   '       <h2> Impact of the number of occurences : </h2>\n'
-                   '     <img src="top1_accuracy_per_occ.jpg">\n'
-                   '     <img src="top3_accuracy_per_occ.jpg">\n'
-                   '    </div>\n')
-        file.write('    <div> \n'
-                   '       <h2> TNSE Embedding : </h2>\n'
-                   '     <img src="pca_explained_ratio.jpg">\n')
-        for i in highlighted_classes: 
-            file.write('     <img src="tnse_class_{}.jpg">\n'.format(i))
-        file.write('    </div>\n')
+#     with open(os.path.join(output_dir, 'error_analysis.html'),'w') as file: 
+#         file.write('<!DOCTYPE html>\n'
+#                    '<html lang="fr">\n'
+#                    '  <head>\n'
+#                    '    <meta charset="utf-8">\n'
+#                    '    <title>Error_Analysis | {}</title>\n'
+#                    '  </head>\n'
+#                    '  <body>\n'
+#                    '    <div> \n'
+#                    '       <h1> Error_Analysis | {} </h1>\n'                   
+#                    '       <h2> Global Metrics : </h2>\n'
+#                    '        <p> Acc: {} <br /> Top3 acc : {}  <br /> '
+#                    '       </p>\n'
+#                    '    </div>\n'.format(run_dir.split('/')[-1],
+#                                          run_dir.split('/')[-1],
+#                                          get_accuracy(result,1), get_accuracy(result,3)
+#                                         ))
+#         file.write('    <div> \n'
+#                    '       <h2> Impact of the number of occurences : </h2>\n'
+#                    '     <img src="top1_accuracy_per_occ.jpg">\n'
+#                    '     <img src="top3_accuracy_per_occ.jpg">\n'
+#                    '    </div>\n')
+#         file.write('    <div> \n'
+#                    '       <h2> TNSE Embedding : </h2>\n'
+#                    '     <img src="pca_explained_ratio.jpg">\n')
+#         for i in highlighted_classes: 
+#             file.write('     <img src="tnse_class_{}.jpg">\n'.format(i))
+#         file.write('    </div>\n')
         
-        file.write('    <div> \n'
-                   '       <h2> Grad-CAM visualizations : </h2>\n')
-        for index in range(5): 
-            file.write('     <img src="grad_cam_{}.jpg">\n'.format(index))
-        file.write('    </div>\n')
+#         file.write('    <div> \n'
+#                    '       <h2> Grad-CAM visualizations : </h2>\n')
+#         for index in range(5): 
+#             file.write('     <img src="grad_cam_{}.jpg">\n'.format(index))
+#         file.write('    </div>\n')
         
-        file.write('  </body>\n'
-                   '</html>\n')
+#         file.write('  </body>\n'
+#                    '</html>\n')
     
 
     
