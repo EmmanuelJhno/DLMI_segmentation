c781e4d74e76d6f034dcfd6d60d74858c7fed8df
S.Brandeis <s.brandeis@orange.fr>
2020-03-29 16:11:53 +0200

    Change num_workers to 6

--- utils/train.sh
+++ utils/train.sh
@@ -2,6 +2,6 @@
 batch_sizes=(2 3 4 8)
 for bs in "${batch_sizes[@]}"; do
   echo "Starting training for $1; batch size = $bs"
-  python ./train.py --config_file ../configs/"$1/bs_$bs.json" --logdir ../runs/"$1" --num_workers 8 --seed 21
+  python ./train.py --config_file ../configs/"$1/bs_$bs.json" --logdir ../runs/"$1" --num_workers 6 --seed 21
   sleep 4
 done
